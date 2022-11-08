dea116d3687821da588984f5731c133ed75df6e0
Soukthavilay <Soukthavilay2000@gmail.com>
2022-11-09 00:31:38 +0700

    Fix button product

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -1,60 +1,66 @@
-import React, {useContext, useState, useEffect} from 'react'
-import {useParams, Link} from 'react-router-dom'
-import {GlobalState} from '../../../GlobalState'
-import ProductItem from '../utils/productItem/ProductItem'
-
+import React, { useContext, useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { GlobalState } from '../../../GlobalState';
+import ProductItem from '../utils/productItem/ProductItem';
 
 function DetailProduct() {
-    const params = useParams()
-    const state = useContext(GlobalState)
-    const [products] = state.productsAPI.products
-    const addCart = state.userAPI.addCart
-    const [detailProduct, setDetailProduct] = useState([])
+  const params = useParams();
+  const state = useContext(GlobalState);
+  const [products] = state.productsAPI.products;
+  const addCart = state.userAPI.addCart;
+  const [detailProduct, setDetailProduct] = useState([]);
 
-    useEffect(() =>{
-        if(params.id){
+  useEffect(() => {
+    if (params.id) {
+      products.forEach((product) => {
+        if (product._id === params.id) setDetailProduct(product);
+      });
+    }
+  }, [params.id, products]);
 
-            products.forEach(product => {
-                if(product._id === params.id) setDetailProduct(product)
-            })
-        }
-    },[params.id, products])
+  if (detailProduct.length === 0) return null;
 
-    if(detailProduct.length === 0) return null;
+  return (
+    <>
+      <div className="detail">
+        <img src={detailProduct.images.url} alt="" />
+        <div className="box-detail">
+          <div className="row">
+            <h3>{detailProduct.title}</h3>
+            <h6>#id: {detailProduct.product_id}</h6>
+          </div>
+          <p>****** [customer review]</p>
+          <p>Đã Bán: {detailProduct.sold}</p>
+          <div className="underline"></div>
+          <br />
+          <span>$ {detailProduct.price}</span>
+          <p>{detailProduct.description}</p>
 
-    return (
-        <>
-            <div className="detail">
-                <img src={detailProduct.images.url} alt="" />
-                <div className="box-detail">
-                    <div className="row">
-                        <h2>{detailProduct.title}</h2>
-                        <h6>#id: {detailProduct.product_id}</h6>
-                    </div>
-                    <span>$ {detailProduct.price}</span>
-                    <p>{detailProduct.description}</p>
-                    <p>{detailProduct.content}</p>
-                    <p>Đã Bán: {detailProduct.sold}</p>
-                    <Link to="/cart" className="cart"
-                    onClick={() => addCart(detailProduct)}>
-                        Mua Ngay
-                    </Link>
-                </div>
-            </div>
+          <Link
+            to="/cart"
+            className="cart"
+            onClick={() => addCart(detailProduct)}
+          >
+            Add to cart
+          </Link>
+          <div>
+            <p>Categories : {detailProduct.category}</p>
+          </div>
+        </div>
+      </div>
 
-            <div>
-                <h2>Sản Phẩm Liên Quan</h2>
-                <div className="products">
-                    {
-                        products.map(product => {
-                            return product.category === detailProduct.category 
-                                ? <ProductItem key={product._id} product={product} /> : null
-                        })
-                    }
-                </div>
-            </div>
-        </>
-    )
+      <div>
+        <h2>Sản Phẩm Liên Quan</h2>
+        <div className="products">
+          {products.map((product) => {
+            return product.category === detailProduct.category ? (
+              <ProductItem key={product._id} product={product} />
+            ) : null;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default DetailProduct
+export default DetailProduct;
